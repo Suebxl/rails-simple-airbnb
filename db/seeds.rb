75a62598b9bfb76108eb12794246f08c8e55aef1
Suzanne Tordjman <suzannetor@gmail.com>
2022-04-16 12:24:36 -0400

    add show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,8 @@ Flat.create(
   address: '10 Clifton Gardens London W9 1DT',
   description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
   price_per_night: 75,
-  number_of_guests: 3
+  number_of_guests: 3,
+  picture_url: "https://images.unsplash.com/photo-1502672260266-1c1ef2d93688?w=1200"
 )
 
 Flat.create(
@@ -22,7 +23,8 @@ Flat.create(
   address: '5 Queensmill Road London SW6 6JP',
   description: 'Lovely warm comfortable and stylishly furnished house. Private bedroom and bathroom with shared living areas.',
   price_per_night: 65,
-  number_of_guests: 2
+  number_of_guests: 2,
+  picture_url: "https://images.unsplash.com/photo-1493809842364-78817add7ffb?w=1200"
 )
 
 Flat.create(
@@ -30,7 +32,8 @@ Flat.create(
   address: 'Euston Rd London N1C 4QP',
   description: 'A mini apartment within the clock tower apartment at St Pancras Station, with its own en suite bathroom and kitchen and sitting area.',
   price_per_night: 110,
-  number_of_guests: 2
+  number_of_guests: 2,
+  picture_url:"https://images.unsplash.com/photo-1494526585095-c41746248156?w=1200"
 )
 
 Flat.create(
@@ -38,7 +41,8 @@ Flat.create(
   address: '93 The Broadway, Wimbledon, London',
   description: 'A stunning two bedroom, two bathroom luxury apartment set in a prestigious modern development near all the amenities of Wimbledon town centre.',
   price_per_night: 195,
-  number_of_guests: 4
+  number_of_guests: 4,
+  picture_url:"https://unsplash.com/photos/syhl9e4igVs"
 )
 
 puts "finished"
